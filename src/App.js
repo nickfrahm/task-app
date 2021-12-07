38bec0744c6d3fd6f40f14390366981aa78f8e09
nickfrahm <nfrahm24@gmail.com>
2021-12-06 19:11:33 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Overview from './components/Overview'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Overview></Overview>
+      <input id='taskInput'></input>
+      <button id='submitBtn'>Submit</button>
     </div>
   );
 }
